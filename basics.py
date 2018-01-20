46e63e050f5108e38384533f831b50f01aa09ff3
maremun <maremun@gmail.com>
2018-01-20 23:40:21 +0300

    Change notation in utility functions.

--- basics.py
+++ basics.py
@@ -8,74 +8,47 @@ from math import sqrt
 from numba import jit
 
 
-def pad_data(x, z, d1, n):
-    pad = lambda xz: np.lib.pad(xz, ((0, 0), (0, d1-n)), 'constant', \
-            constant_values=(0))
+def pad_data(x, d1, d):
+    pad = lambda z: np.lib.pad(z, ((0, 0), (0, d1-d)),
+                               'constant', constant_values=(0))
     x = pad(x)
-    z = pad(z)
-    return x, z
+    return x
 
 
 @jit(nopython=True)
-def radius(n, k):
-    h = int(np.ceil(k))
-    rm = n + 2
+def radius(d, n):
+    h = int(np.ceil(n))
+    rm = d + 2
     r = np.sqrt(2 * np.random.gamma(rm/2., 1., h))
     return r
 
 
 @jit(nopython=True)
-def get_d0(k, n):
+def get_D(d, n):
     '''
     when using simplex method to generate points, we add also reflected ones,
     so the overall number of points created is 2 * (n+1).
     '''
-    d0 = int(2 * k * (n+1))
-    return d0
+    D = int(2 * n * (d+1))
+    return D
 
 
-def generate_random_weights(k, n):
-    d0 = get_d0(k, n)
-    M = np.random.randn(d0, n)
-    return M, None
-
-
-@jit(nopython=True)
-def simplex_matvec(x):
+def generate_random_weights(d, n):
     '''
-    V.T @ x (V is n-simplex)
+    Generates random Fourier features
+
+    Args
+        d: the input dimension of data
+        n: total number of features is D = 2*n(d+1)
+
+    Returns:
+        M: the points to estimate integral with
+        w: the weights of the points in M
     '''
-    n = x.shape[0]
-    mp = n + 1
-    r = np.empty(n + 1)
-    rv = np.empty(n)
-    s = 0.
-    for i in range(n):
-        rv[i] = sqrt(mp / ((n - i) * n * (n - i + 1.))) * x[i]
-        r[i] = s + rv[i] * (n-i)
-        s += -rv[i]
-    r[n] = s
-
-    return r
-
-
-@jit(nopython=True)
-def batch_simplex_matvec(x):
-    nobj = x.shape[1]
-    n = x.shape[0]
-    mp = n + 1
-    r = np.empty((n+1, nobj))
-    rv = np.empty(n)
-    s = np.zeros(nobj)
-    for i in range(n):
-        rv[i] = sqrt(mp / ((n - i) * n * (n - i + 1.)))
-        for o in range(nobj):
-            rvo = rv[i] * x[i, o]
-            r[i, o] = s[o] + rvo * (n-i)
-            s[o] += -rvo
-    for o in range(nobj):
-        r[n, o] = s[o]
-    return r
+    D = get_D(n, d)
+    M = np.random.randn(D, d)
+    w = None  # all points have equal weights
+    return M, w
 
 
 @jit(nopython=True)
@@ -88,3 +61,41 @@ def rnsimp(m):
         S[i, i+1:m+1] = -rv
 
     return S
+
+
+@jit(nopython=True)
+def simplex_matvec(x):
+    '''
+    V.T @ x (V is n-simplex)
+    '''
+    d = x.shape[0]
+    mp = d + 1
+    r = np.empty(d + 1)
+    rv = np.empty(d)
+    s = 0.
+    for i in range(d):
+        rv[i] = sqrt(mp / ((d-i) * d * (d-i+1.))) * x[i]
+        r[i] = s + rv[i] * (d-i)
+        s += -rv[i]
+    r[d] = s
+
+    return r
+
+
+@jit(nopython=True)
+def batch_simplex_matvec(x):
+    nobj = x.shape[1]
+    d = x.shape[0]
+    mp = d + 1
+    r = np.empty((d+1, nobj))
+    rv = np.empty(d)
+    s = np.zeros(nobj)
+    for i in range(d):
+        rv[i] = sqrt(mp / ((d-i) * d * (d-i+1.)))
+        for o in range(nobj):
+            rvo = rv[i] * x[i, o]
+            r[i, o] = s[o] + rvo * (d-i)
+            s[o] += -rvo
+    for o in range(nobj):
+        r[d, o] = s[o]
+    return r
