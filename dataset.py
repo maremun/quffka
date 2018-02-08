bb9dbd4b2a1004c37c4f211b2327fd260dc93df4
maremun <maremun@gmail.com>
2018-02-09 00:09:33 +0300

    Add make_dataset function for ease of experiments.

--- dataset.py
+++ dataset.py
@@ -4,6 +4,17 @@
 
 import numpy as np
 
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+PARAMS = {'USPS': [1, 5, 500, 0, 1, 550, 4500],
+          'Powerplant': [1, 5, 500, 0, 1, 550, 8500],
+          'LETTER': [1, 5, 500, 0, 1, 550, 10000],
+          'MNIST': [1, 5, 50, 0, 1, 550, 50000],
+          'CIFAR100': [1, 5, 50, 0, 1, 550, 50000],
+          'LEUKEMIA': [1, 5, 50, 0, 1, 550, 38]
+          }
+
 
 def pack_dataset(data, labels, n):
     xtest = data[n:]
@@ -14,10 +25,11 @@ def pack_dataset(data, labels, n):
     return [xtrain, ytrain, xtest, ytest], d
 
 
-def get_dataset(name, n):
+def load_dataset(name, n):
     path = 'datasets/%s/' % name
     data = np.load(path+'data.npy')
     labels = np.load(path+'labels.npy')
+
     return pack_dataset(data, labels, n)
 
 
@@ -29,3 +41,34 @@ def sample_dataset(nsamples, dataset):
     pick = np.random.choice(idx, nsamples, False)
     sampled = xtrain[pick, :]
     return sampled
+
+
+def scale_data(xtrain, xtest):
+    xtrain = xtrain.astype('float')
+    xtest = xtest.astype('float')
+    xtrain /= xtrain.max()
+    xtest /= xtest.max()
+    return xtrain, xtest
+
+
+def make_dataset(dataset_name, params=None):
+    if params is None:
+        params = PARAMS[dataset_name]
+    start_deg, max_deg, runs, shift, step, NSAMPLES, delimiter = params
+
+    if dataset_name == 'MNIST':
+        mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+        xtrain = mnist.train.images
+        ytrain = mnist.train.labels
+        xtest = mnist.test.images
+        ytest = mnist.test.labels
+        d = xtrain.shape[1]
+    else:
+        dataset, d = load_dataset(dataset_name, delimiter)
+        xtrain, ytrain, xtest, ytest = dataset
+
+    xtrain, xtest = scale_data(xtrain, xtest)
+    dataset = [xtrain, ytrain, xtest, ytest]
+    X = sample_dataset(NSAMPLES, dataset)
+    Y = sample_dataset(NSAMPLES, dataset)
+    return X, Y, params
