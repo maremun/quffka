4e59438705bfecb78073d09989a83a7e8da46601
maremun <maremun@gmail.com>
2018-04-15 16:01:52 +0300

    Fix setup.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #   encoding: utf8
 #   setup.py
 
+from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
+
+
+def load_requirements(fname):
+    return [str(r.req) for r in parse_requirements(fname, session='')]
 
 
 setup(name='quffka',
       version='0.1.0',
       packages=find_packages(),
+      install_requires=load_requirements('requirements.txt'),
       entry_points={
           'console_scripts': [
               'cook-datasets = quffka.cook_datasets:main',
